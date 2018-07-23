854f55ce37ab0578d15b03718ffa9324df22e6ed
JennaBarbara <jennabarbara@gmail.com>
2018-07-22 23:24:31 -0400

    Edit mode works

--- src/App.js
+++ src/App.js
@@ -27,14 +27,12 @@ class Step extends Component {
   }
   tick(){
    if(this.state.currenttime>0){
-     console.log("this.state.currenttime");
      this.setState({
        currenttime: this.state.currenttime-1
      });
    }
    else{
       clearInterval(this.timerInterval);
-      console.log("clearInterval");
    }
 
    }
@@ -66,7 +64,7 @@ class Info extends Component {
           Insert the plunger into the Aeropress and place it on a stable surface filter-side up. Add the coffee grounds into the Aeropress well.
          </p>
          <button onClick={this.props.nextMode} className="App-button">Start</button>
-         <button className="App-button">Edit</button>
+         <button onClick={this.props.editMode} className="App-button">Edit</button>
          <p>
          <b>{this.props.Content[0][0]}</b>
          </p>
@@ -116,22 +114,33 @@ class App extends Component {
            "Pour in the remaining water and let sit until the timer is up.",
            60],
           ["Flip",
-           "Screw in the fliter to your Aeropress and place your cup on top. Carefully flip the entire arrangement.",
-           10],
+           "Screw in the fliter to your Aeropress and place your cup on top. Carefully flip the entire arrangement.", 0
+           ],
           ["Plunge",
            "Firmly press down the plunger. You should take up the time remaining time on the timer before you hear the gentle hiss sound and you know your coffee is ready.",
            20],
           ["Done",
           "Your coffee is done, have a nice rest of your day."]
       ],
-    mode: 0
+      newTime : [ 20, 60,0, 20],
+      mode: 0
    }
   this.nextMode = this.nextMode.bind(this);
+  this.editMode = this.editMode.bind(this);
+  this.editTime = this.editTime.bind(this);
   this.Cancel = this.Cancel.bind(this);
+   this.handleChange = this.handleChange.bind(this);
   }
-
+  handleChange(event) {
+    var changeHandler = Object.assign({}, this.state.newTime);
+    changeHandler[[event.target.name]] = event.target.value;
+    this.setState({newTime : changeHandler});
+  }
+ editMode() {
+   var edit = "edit";
+   this.setState({mode:edit});
+ }
   nextMode() {
-
     var next;
     if(this.state.mode === 5){
       next = 0;
@@ -149,11 +158,23 @@ class App extends Component {
      if(this.state.mode===0){
        return this.renderInfo( );
      }
-     if(this.state.mode > 0){
+     else if(this.state.mode > 0){
        return this.renderPrime( );
      }
+     else if(this.state.mode=="edit"){
+       return this.renderEdit();
+     }
    }
+  editTime() {
+    var newContent = Object.assign({}, this.state.content);
+    newContent[0][2] = this.state.newTime[0];
+    newContent[1][2] = this.state.newTime[1];
+    newContent[2][2] = this.state.newTime[2];
+    newContent[3][2] = this.state.newTime[3];
+   var newMode = 0;
 
+    this.setState({ content : newContent, mode : newMode });
+  }
 
    renderInfo( ) {
    return(
@@ -164,7 +185,7 @@ class App extends Component {
           For the inverted brewing method
          </p>
        </header>
-       <Info nextMode={this.nextMode} Content = {this.state.content}></Info>
+       <Info editMode={this.editMode} nextMode={this.nextMode} Content = {this.state.content}></Info>
       </div>
      );
    }
@@ -174,13 +195,34 @@ class App extends Component {
             <header className="App-header">
               <h1 className="App-title">Aeropress Timer</h1>
               <p className="App-intro">
-               Prime
+               For the inverted brewing method
               </p>
             </header>
             <Step Cancel = {this.Cancel} nextMode={this.nextMode} Mode ={this.state.mode} Content = {this.state.content}></Step>
            </div>
           );
         }
+  renderEdit() {
+      return(
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Aeropress Timer</h1>
+          <p className="App-intro">
+          For the inverted brewing method
+          </p>
+        </header>
+        <form onSubmit={this.editTime}>
+        <input type="number" name="0" min="0" value={this.state.newTime[0]}  onChange={this.handleChange}/>
+        <input type="number" name="1" min="0" value={this.state.newTime[1]}  onChange={this.handleChange}/>
+        <input type="number" name="2" min="0" value={this.state.newTime[2]}  onChange={this.handleChange}/>
+        <input type="number" name="3" min="0" value={this.state.newTime[3]}  onChange={this.handleChange}/>
+        <button type="submit" value="Submit" className="App-button">Save</button>
+        </form>
+
+        </div>
+      );
+
+    }
 
 }
 
